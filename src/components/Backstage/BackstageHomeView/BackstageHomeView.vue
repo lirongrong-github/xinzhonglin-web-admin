26f137f93cd3a92e7b4708545d8d19f3e1c4beab
Lirongrong <lrw156804437@outlook.com>
2020-08-11 17:39:03 +0800

    lrr

--- src/components/Backstage/BackstageHomeView/BackstageHomeView.vue
+++ src/components/Backstage/BackstageHomeView/BackstageHomeView.vue
@@ -8,6 +8,16 @@
         <div class="grid-content bg-purple-light">4</div>
       </el-col>
     </el-row>
+
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+    
   </div>
 </template>
 
